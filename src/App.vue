9e03136ece9f1487a37be1d5316fedaa7b6993d5
Grace Hopper <ghopp01@gmail.com>
2021-09-06 09:24:25 -0500

    Add new folders and files for css and js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your New App"/>
 </template>
+
+<style>
+  @import './assets/styles/basic.css';
+</style>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
