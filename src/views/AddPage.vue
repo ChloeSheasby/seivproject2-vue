66242eb2b8cde202e3399d6a33bffa07808b681a
ChloeSheasby <47607144+ChloeSheasby@users.noreply.github.com>
2021-09-14 17:29:18 -0500

    Revert "Revert 11 add router cs"

---
+++ src/views/AddPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!--<UserDisplay></UserDisplay>-->
+    <h1>Add Course with Vue</h1>
+  </div>
+</template>
+<style>
+  @import '@/assets/styles/basic.css';
+</style>
+
+<script>
+/*import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}*/
+
+</script>
